e121186e2d46c057b998c52674968daceab5a063
ganmao <ganmao@cd921824-a84c-246f-e7b0-06b4e6d47db8>
2010-12-07 05:16:00 +0000

    建立0.2的基线版本

---
+++ setup.py
@@ -0,0 +1,33 @@
+# coding:utf-8
+'''
+Created on 2010-11-28
+
+@author: zdl
+'''
+
+from distutils.core import setup
+import py2exe
+import glob
+
+options = {"py2exe":
+           {"compressed": True,
+            "optimize": 2,
+            "bundle_files": 1,
+            #"dll_excludes" : ['msvcr71.dll'],
+            "excludes" : ['_ssl', 'doctest', 'pdb', 'unittest', 'difflib',
+                          'optparse', 'pickle', 'calebdar', 'inspect']
+            }
+           }
+
+setup(version = "0.2",
+      description = "Diameter Message Tools",
+      name = "Diameter Message Tools",
+      author = "zhang.dongliang",
+      author_email = "zhang.dongliang@zte.com.cn",
+      options = options,
+      zipfile=None,
+      windows=[{"script":"DccTranslate.pyw",
+                "icon_resources":[(1, "kachi.ico")]}
+              ],
+      data_files=[("etc", glob.glob("etc/*.ini")),
+                 ]) 
